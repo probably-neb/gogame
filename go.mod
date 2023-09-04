d55eee5bc6d22fcd665a3e40532c09dbfb052cbc
Ben Kunkle <ben.kunkle@gmail.com>
2023-09-04 12:54:48 -0700

    make counter work using websocket

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
+module nebsite.website/gogame
+
+go 1.21.0
 
 require (
 	github.com/gorilla/websocket v1.5.0 // indirect
--- server.go
+++ server.go
@@ -1,24 +1,53 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
-    "fmt"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
 
 func main() {
-    var count int = 0;
+	var count int = 0
 	// Set routing rules
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
 	})
-    http.HandleFunc("/count", func(w http.ResponseWriter, r *http.Request) {
-        count++
-        fmt.Fprintf(w, "%d", count)
-    })
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		// upgrade this connection to a WebSocket
+		ws, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println(err)
+		}
 
-    fmt.Println("Server started at 8080 port")
+		log.Println("Client Connected")
+		for {
+			// read in a message
+			messageType, p, err := ws.ReadMessage()
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			// print out that message for clarity
+			log.Println("WebSocket Message Received: [", messageType,"]", string(p))
+
+            count++;
+			msg := []byte(fmt.Sprintf("<div id=\"count\">%d</div>", count))
+			err = ws.WriteMessage(websocket.TextMessage, msg)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+		}
+	})
+
+	fmt.Println("Server started at 8080 port")
 	//Use the default DefaultServeMux.
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
