448ddef5de6a49895a0a84a3275cae6b4aaac282
Ben Kunkle <ben.kunkle@gmail.com>
2023-09-04 12:18:54 -0700

    simple counter using htmx

--- server.go
+++ server.go
@@ -3,14 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+    "fmt"
 )
 
+
 func main() {
+    var count int = 0;
 	// Set routing rules
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
 	})
+    http.HandleFunc("/count", func(w http.ResponseWriter, r *http.Request) {
+        count++
+        fmt.Fprintf(w, "%d", count)
+    })
 
+    fmt.Println("Server started at 8080 port")
 	//Use the default DefaultServeMux.
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
