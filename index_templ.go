dc20e801cee359b80c54e8cbefa23073b51d70e3
Ben Kunkle <ben.kunkle@gmail.com>
2023-09-11 09:19:11 -0700

    make cancelling room join redirect to /rooms

--- index_templ.go
+++ index_templ.go
@@ -297,7 +297,7 @@ func JoinRoomModal() templ.Component {
 			if err != nil {
 				return err
 			}
-			_, err = templBuffer.WriteString("</button><button type=\"button\" class=\"mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto\" hx-get=\"/ok\" hx-swap=\"delete\" hx-target=\"#join-room-modal\">")
+			_, err = templBuffer.WriteString("</button><button type=\"button\" class=\"mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto\"><a href=\"/rooms\" hx-boost=\"true\">")
 			if err != nil {
 				return err
 			}
@@ -306,7 +306,7 @@ func JoinRoomModal() templ.Component {
 			if err != nil {
 				return err
 			}
-			_, err = templBuffer.WriteString("</button></form>")
+			_, err = templBuffer.WriteString("</a></button></form>")
 			if err != nil {
 				return err
 			}
